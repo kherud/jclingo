8a4f3376614721bf1f1d6c8e165b08dc55369771
Josef Schneeberger <josef@dr-schneeberger.de>
2021-05-02 11:33:46 +0200

    Added MIT License to Clingo class

--- src/main/java/org/potassco/base/Clingo.java
+++ src/main/java/org/potassco/base/Clingo.java
@@ -10,28 +10,44 @@ import org.potassco.enums.ExternalType;
 import org.potassco.enums.HeuristicType;
 import org.potassco.enums.ModelType;
 import org.potassco.enums.ShowType;
-import org.potassco.enums.SolveMode;
 import org.potassco.enums.StatisticsType;
 import org.potassco.enums.SymbolType;
 import org.potassco.enums.TermType;
-import org.potassco.enums.TruthValue;
 import org.potassco.jna.ClingoLibrary;
 import org.potassco.jna.OptionParseCallbackT;
-import org.potassco.jna.Part;
 import org.potassco.jna.Size;
 import org.potassco.jna.SizeByReference;
-import org.potassco.jna.SolveEventCallbackT;
 import org.potassco.jna.SymbolByReference;
 
 import com.sun.jna.Pointer;
-import com.sun.jna.StringArray;
 import com.sun.jna.ptr.ByteByReference;
 import com.sun.jna.ptr.DoubleByReference;
 import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.LongByReference;
 import com.sun.jna.ptr.PointerByReference;
 
-/**
+/** MIT License</br>
+ * Copyright 2021 Josef Schneeberger</br>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:</br>
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.</br>
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * <p>
  * Clingo is a singleton and holds the reference to the clingoLibrary.
  * @author Josef Schneeberger
  *

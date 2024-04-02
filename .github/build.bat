0d04e4eeaed438520c5447e2005c96a9dfff5694
Konstantin Herud <konstantin.herud@gmail.com>
2024-04-02 23:46:09 +0200

    Update build.bat

--- .github/build.bat
+++ .github/build.bat
@@ -11,6 +11,9 @@ cmake -H. -Bbuild %* ^
   -DCLINGO_MANAGE_RPATH=OFF ^
   -DCLINGO_BUILD_SHARED=ON
 
-cmake --build build --config Release
+cmake --build build ^
+      --config Release ^
+      --target libclingo ^
+      -j $(nproc)
 
 if errorlevel 1 exit /b %ERRORLEVEL%
